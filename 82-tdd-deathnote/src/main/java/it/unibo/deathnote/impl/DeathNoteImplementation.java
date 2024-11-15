2d4da7983feddc9ffa23144aa632d92dff38e1ca
Cristian Di Donato <cristian.didonato@studio.unibo.it>
2024-11-15 15:37:15 +0100

    The method 'testGetRules()' for testing the method 'getRule()' has been added

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -0,0 +1,10 @@
+package it.unibo.deathnote.impl;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImplementation implements DeathNote{
+
+    public DeathNoteImplementation(){
+
+    }
+}
--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,34 @@
 package it.unibo.deathnote;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-class TestDeathNote {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Assertions;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImplementation;
+
+class TestDeathNote{
+    final static String DEFAULT_VICTIM = "Mario Rossi";
+    final DeathNote dbook = new DeathNoteImplementation();
+
+    @BeforeEach
+    void initialize(){
+        dbook.writeName(DEFAULT_VICTIM);
+    }
+
+    @Test
+    void tesGetRules(){
+        Exception e = assertThrows(IllegalArgumentException.class, () -> dbook.getRule(0));
+        assertNotNull(e.getMessage());
+        assertFalse(e.getMessage().isEmpty() || e.getMessage().isBlank());
+        e = assertThrows(IllegalArgumentException.class, () -> dbook.getRule(-1));
+        assertNotNull(e.getMessage());
+        assertFalse(e.getMessage().isEmpty() || e.getMessage().isBlank());
+
+    }
 }
